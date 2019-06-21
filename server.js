826137e33164e21ef599a26e7a205db48e8db34c
Sawyer Burnett <sawyerburnett@gmail.com>
2019-06-20 19:28:12 -0700

    switch to yarn. add logging to tell port

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-
 //required modules
-let fs = require('fs');
-let express = require('express');
+let fs = require("fs");
+let express = require("express");
 let app = express();
-let mongoose = require('mongoose');
-let bodyParser = require('body-parser');
+let mongoose = require("mongoose");
+let bodyParser = require("body-parser");
 let configuration;
 
 //bodyparser stuff
-app.use(bodyParser.json()); 
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use('/', express.static(__dirname + '/public'));
-require('./routes')(app);
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", express.static(__dirname + "/public"));
+require("./routes")(app);
 exports = app;
 
 //constants
-const PORT=8080;
-const settings = 'settings.json'; 
-const ERROR_TEXT = "You must have a database string in the settings.json file or this app will not work!";
+const PORT = 8080;
+const settings = "settings.json";
+const ERROR_TEXT =
+  "You must have a database string in the settings.json file or this app will not work!";
 
 if (fs.existsSync(settings)) {
   //read config. values from settings.json configuration file
@@ -33,4 +33,6 @@ if (fs.existsSync(settings)) {
 mongoose.Promise = global.Promise;
 mongoose.connect(configuration.mongoUrl, { useMongoClient: true });
 
-app.listen(PORT);
+app.listen(PORT, function() {
+  console.log(`listening on ${PORT}`);
+});
